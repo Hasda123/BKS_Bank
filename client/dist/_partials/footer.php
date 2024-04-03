477820bd53f54048f25a33bb86f9cc790ddb76cf
Soorjyakanta Sethy <soorjyakant17@gmail.com>
2024-04-03 22:04:20 +0530

    Create footer.php

---
+++ client/dist/_partials/footer.php
@@ -0,0 +1,16 @@
+<?php
+/* Persisit System Settings On Brand */
+$ret = "SELECT * FROM `iB_SystemSettings` ";
+$stmt = $mysqli->prepare($ret);
+$stmt->execute(); //ok
+$res = $stmt->get_result();
+while ($sys = $res->fetch_object()) {
+?>
+  <footer class="main-footer">
+    <strong>&copy; 2023-<?php echo date('Y'); ?> - Crafted with passion.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 2.0.1
+    </div>
+  </footer>
+<?php } ?>
